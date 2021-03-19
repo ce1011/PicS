d134b7885917db8fe427534ee4ffcc0503fd8b6b
Parco Au-Yeung <paco20071001@gmail.com>
2021-03-19 14:55:52 +0800

    Integrate Profile Page with firestore

--- lib/page/home/Home_Page_Profile.dart
+++ lib/page/home/Home_Page_Profile.dart
@@ -31,56 +31,52 @@ class HomePageProfile extends StatelessWidget {
     return profilePostList;
   }
 
+  Future<QueryDocumentSnapshot> getProfileInformation(String UID) async {
+    QueryDocumentSnapshot profileInformation;
+
+    CollectionReference profile = firestoreInstance.collection("user");
+
+    await profile
+        .where('UID', isEqualTo: UID)
+        .get()
+        .then((data) => profileInformation = data.docs[0]);
+
+    return profileInformation;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
         child: Column(
       children: [
-        Expanded(
-            flex: 3,
-            child: Column(
-              children: [
-                Container(
-                  padding: EdgeInsets.only(top: 10.0),
-                  child: ListTile(
-                      leading: CircleIcon(
-                          url:
-                              "https://pbs.twimg.com/profile_images/1343584679664873479/Xos3xQfk_400x400.jpg"),
-                      title: Text(Provider.of<LoginStateNotifier>(context, listen: false).UID),
-                      subtitle: Text("handsome123")),
-                ),
-                ListTile(subtitle: Text("Lorem ipsum dolor sit amet")),
-                RaisedButton(onPressed: ()async {
-                  CollectionReference comment =
-                  FirebaseFirestore.instance.collection('/post/mLGYRFf9B5DKhh6zybFq/comment');
-
-                  int lastIndex;
-                  bool success;
-
-                  QuerySnapshot lastComment = await comment
-                      .orderBy('commentID', descending: true)
-                      .limit(1)
-                      .get();
-
-                  lastIndex = lastComment.docs[0].data()['commentID'];
-
-
-                  await comment
-                      .add({
-                    'commentID': lastIndex+1,
-                    'UID': "testest",
-                    'commentTime': new Timestamp.now(),
-                    'content': "testest",
-                    'startX': 200,
-                    'startY': 200,
-                    'endX': 500,
-                    'endY': 500
-                  })
-                      .then((value) => success = true)
-                      .catchError((error) => success = false);
-                })
-              ],
-            )),
+        FutureBuilder(
+            future: getProfileInformation(
+                Provider.of<LoginStateNotifier>(context, listen: false).UID),
+            builder: (BuildContext context,
+                AsyncSnapshot<QueryDocumentSnapshot> snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                if (snapshot.hasError) {
+                  return Text("Error");
+                } else {
+                  return Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.only(top: 10.0),
+                        child: ListTile(
+                            leading: CircleIcon(
+                                url:
+                                "https://pbs.twimg.com/profile_images/1343584679664873479/Xos3xQfk_400x400.jpg"),
+                            title: Text(snapshot.data['displayName']),
+                            subtitle: Text(snapshot.data['username'])),
+                      ),
+                      ListTile(subtitle: Text(snapshot.data['description'])),
+                    ],
+                  );
+                }
+              } else {
+                return CircularProgressIndicator();
+              }
+            }),
         Divider(
           color: Colors.greenAccent[400],
         ),
@@ -94,7 +90,7 @@ class HomePageProfile extends StatelessWidget {
                 if (snapshot.hasError) {
                   return Text("Error");
                 } else {
-                  return Expanded(
+                  return snapshot.data.isEmpty ? Text("No post"): Expanded(
                     flex: 7,
                     child: SingleChildScrollView(
                       child: Column(
@@ -110,7 +106,9 @@ class HomePageProfile extends StatelessWidget {
                             child: Text("Log Out"),
                             onPressed: () {
                               auth.signOut();
-                              Provider.of<LoginStateNotifier>(context, listen: false).logout();
+                              Provider.of<LoginStateNotifier>(context,
+                                      listen: false)
+                                  .logout();
                               Navigator.popAndPushNamed(context, "/");
                             },
                           ),
